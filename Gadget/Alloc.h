59437c10b5c74336a05164036629509e7885e39b
292236116 <292236116@qq.com>
2018-12-08 17:44:30 +0800

    push

---
+++ Gadget/Alloc.h
@@ -0,0 +1,223 @@
+﻿#pragma once
+//
+//  Alloc.h
+//  HomeWork
+//
+//  Created by 刘泽滨 on 2018/12/5.
+//  Copyright © 2018年 刘泽滨. All rights reserved.
+//
+
+#ifndef Alloc_h
+#define Alloc_h
+/*
+ （1）用C语言分别实现采用首次适应算法和最佳适应算法的动态分区分配过程alloc( )和回收过程free( )。其中，空闲分区通过空闲分区链来管理：在进行内存分配时，系统优先使用空闲区低端的空间。
+ （2）假设初始状态下，可用的内存空间为640KB，并有下列的请求序列：
+ •作业1申请130KB。
+ •作业2申请60KB。
+ •作业3申请100KB。
+ •作业2释放60KB。
+ •作业4申请200KB。
+ •作业3释放100KB。
+ •作业1释放130KB。
+ •作业5申请140KB。
+ •作业6申请60KB。
+ •作业7申请50KB。
+ •作业6释放60KB。
+ 请分别采用首次适应算法和最佳适应算法，对内存块进行分配和回收，要求每次分配和回收后显示出空闲分区链的情况。
+
+ */
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <vector>
+struct FirstAvailable {};
+struct BestAdapt {};
+#define KB 1024
+#define MB 1048576
+static char* memStart;
+struct Block {
+	char* start;
+	char* end;
+	inline size_t size()const {
+		return end - start;
+	}
+	Block(char* s, char* e) :start(s), end(e) {}
+};
+
+class BlockCmp {
+public:
+	bool operator()(const Block& l, const Block& r) {
+		return l.size() < r.size();
+	}
+};
+
+std::set<Block, BlockCmp> blockIndex;
+std::vector<Block> blockArray;
+
+void init(char* p) {
+	blockIndex.insert(Block{ p,p + 640 * KB });
+	blockArray.push_back(Block{ p,p + 640 * KB });
+	std::cout << "首地址:0" << std::endl;
+}
+
+void printAddr(void* addr, int num, size_t sz) {
+	std::cout << "Assign" << num << "分配地址:[" << std::hex << (char*)addr - memStart << "," << std::hex << (char*)addr - memStart + sz << "]" << std::endl;
+}
+//alloc
+template<class T>
+void* Malloc(size_t byteNum) {}
+
+template<>
+void* Malloc<FirstAvailable>(size_t byteNum) {
+	auto finded = std::find_if(blockArray.begin(), blockArray.end(), [=](Block& b) {
+		return b.size() >= byteNum;
+	});
+	if (finded != blockArray.end()) {
+		char* res = finded->start;
+		finded->start += byteNum + 1;
+		if (finded->size() == 0) {
+			blockArray.erase(finded);
+		}
+		return res;
+	}
+	else {
+		std::cout << "Alloc Fail\n";
+		return nullptr;
+	}
+}
+template<>
+void* Malloc<BestAdapt>(size_t byteNum) {
+	auto itr = blockIndex.lower_bound(Block{ (char*)0, (char*)byteNum });
+	auto res = itr->start;
+	if (itr != blockIndex.end()) {
+		Block tmp = *itr;
+		tmp.start += byteNum + 1;
+		blockIndex.erase(itr);
+		blockIndex.insert(tmp);
+		return res;
+	}
+	else {
+		std::cout << "Alloc Fail\n";
+		return nullptr;
+	}
+}
+//free
+template<typename T>
+bool Mfree(void* p, size_t byteNum) {}
+
+template<>
+bool Mfree<FirstAvailable>(void* p, size_t byteNum) {
+	char* tmp = (char*)p;
+	blockArray.push_back(Block{ tmp,tmp + byteNum });
+	return true;
+}
+template<>
+bool Mfree<BestAdapt>(void* p, size_t byteNum) {
+	char* tmp = (char*)p;
+	blockIndex.insert(Block{ tmp,tmp + byteNum });
+	return true;
+}
+
+void printArray() {
+	std::cout << "所有可用的内存块\n";
+	int counter = 0;
+	for (const auto& e : blockArray) {
+		counter++;
+		std::cout << "第" << counter << "块:[" << std::hex << e.start - memStart << "," << std::hex << e.end - memStart << "]" << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void printIndex() {
+	std::cout << "所有可用的内存块\n";
+	int counter = 0;
+	for (const auto& e : blockIndex) {
+		counter++;
+		std::cout << "第" << counter << "块:[" << std::hex << e.start - memStart << "," << std::hex << e.end - memStart << "]" << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void first_available() {
+	std::cout << "\nshowing first available----------" << std::endl;
+
+	void* assignment1 = Malloc<FirstAvailable>(130 * KB);
+	printAddr(assignment1, 1, 130 * KB); printArray();
+
+	void* assignment2 = Malloc<FirstAvailable>(60 * KB);
+	printAddr(assignment2, 2, 60 * KB); printArray();
+
+	void* assignment3 = Malloc<FirstAvailable>(100 * KB);
+	printAddr(assignment3, 3, 100 * KB); printArray();
+
+	Mfree<FirstAvailable>(assignment2, 60 * KB);
+	printArray();
+
+	void* assignment4 = Malloc<FirstAvailable>(200 * KB);
+	printAddr(assignment4, 4, 200 * KB); printArray();
+
+	Mfree<FirstAvailable>(assignment3, 100 * KB);
+	printArray();
+
+	Mfree<FirstAvailable>(assignment1, 130 * KB);
+	printArray();
+
+	void* assignment5 = Malloc<FirstAvailable>(140 * KB);
+	printAddr(assignment5, 5, 140 * KB); printArray();
+
+	void* assignment6 = Malloc<FirstAvailable>(60 * KB);
+	printAddr(assignment6, 6, 60 * KB); printArray();
+
+	void* assignment7 = Malloc<FirstAvailable>(50 * KB);
+	printAddr(assignment7, 7, 50 * KB); printArray();
+
+	Mfree<FirstAvailable>(assignment6, 60 * KB);
+	printArray();
+}
+
+void best_adpat() {
+	std::cout << "\nshowing best adapt-----------" << std::endl;
+
+	void* assignment1 = Malloc<BestAdapt>(130 * KB);
+	printAddr(assignment1, 1, 130 * KB); printArray();
+
+	void* assignment2 = Malloc<BestAdapt>(60 * KB);
+	printAddr(assignment2, 2, 60 * KB); printArray();
+
+	void* assignment3 = Malloc<BestAdapt>(100 * KB);
+	printAddr(assignment3, 3, 100 * KB); printArray();
+
+	Mfree<BestAdapt>(assignment2, 60 * KB);
+	printArray();
+
+	void* assignment4 = Malloc<BestAdapt>(200 * KB);
+	printAddr(assignment4, 4, 200 * KB); printArray();
+
+	Mfree<BestAdapt>(assignment3, 100 * KB);
+	printArray();
+
+	Mfree<BestAdapt>(assignment1, 130 * KB);
+	printArray();
+
+	void* assignment5 = Malloc<BestAdapt>(140 * KB);
+	printAddr(assignment5, 5, 140 * KB); printArray();
+
+	void* assignment6 = Malloc<BestAdapt>(60 * KB);
+	printAddr(assignment6, 6, 60 * KB); printArray();
+
+	void* assignment7 = Malloc<BestAdapt>(50 * KB);
+	printAddr(assignment7, 7, 60 * KB); printArray();
+
+	Mfree<BestAdapt>(assignment6, 60 * KB);
+	printArray();
+}
+
+void HomeWork1() {
+	memStart = (char*)malloc(640 * KB);
+	init(memStart);
+	first_available();
+	best_adpat();
+}
+
+#endif /* Alloc_h */
---
+++ Gadget/Pages.h
@@ -0,0 +1,295 @@
+﻿#pragma once
+//
+//  Pages.h
+//  HomeWork
+//
+//  Created by 刘泽滨 on 2018/12/6.
+//  Copyright © 2018年 刘泽滨. All rights reserved.
+//
+
+#ifndef Pages_h
+#define Pages_h
+#define     OPT     1
+#define     FIFO    2
+#define     LRU     3
+using namespace std;
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+vector<int> path;
+deque<int*> memoryQueue;
+int instruction[320];
+int instructions[320];
+int* page[32];
+int pageCounter;
+int pc;
+int numOfMissPage;
+
+int pc_generator(int pc) {
+	int speed = rand() % 100;
+	//before
+	if (speed < 25) {
+		return rand() % (pc + 1);
+	}
+	//after
+	else if (speed < 50) {
+		return pc + (rand() % (319 - pc));
+	}
+	//+1
+	else {
+		return pc = pc + 1 > 319 ? 319 : pc + 1;
+	}
+
+}
+
+int pageNum(int* pi) {
+	return (pi - instructions) / 10;
+}
+
+void reset() {
+	pc = 0;
+	numOfMissPage = 0;
+	pageCounter = 0;
+	memoryQueue.clear();
+}
+
+void init_instruction() {
+	for (int i = 0; i < 320; i++) {
+		instructions[i] = i;
+	}
+	for (int i = 0; i < 320; i++) {
+		instruction[i] = pc_generator(i);
+	}
+	for (int i = 0; i < 32; i++) {
+		page[i] = instructions + i * 10;
+	}
+}
+
+bool is_page_in_memoryQueue() {
+	for (const auto& e : memoryQueue) {
+		for (int i = 0; i < 10; i++) {
+			if (e[i] == instruction[pc]) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+void print_memoryQueue() {
+	cout << "内存中的页:\n";
+	for (const auto& e : memoryQueue) {
+		cout << "第" << pageNum(e) + 1 << "页\t";
+	}
+	cout << endl;
+}
+
+int* find_instruction(int insNum) {
+	//循环展开
+	//    bool tmp1 = false;
+	//    bool tmp2 = false;
+	//    for(int i=0;i!=320;i++){
+	//        tmp1 = instruction[i]==insNum;
+	//        tmp2 = instruction[i+1]==insNum;
+	//    }
+	for (int i = 0; i != 320; i++) {
+		if (instructions[i] == insNum) {
+			return instructions + i;
+		}
+	}
+	return nullptr;
+}
+
+int distance(int pageNum) {
+	int res = 0; auto itr = path.begin() + pageCounter;
+	for (; itr != path.end(); itr++) {
+		if (*itr == pageNum) {
+			return res;
+		}
+		else {
+			res++;
+		}
+	}
+	return res;
+}
+
+int get_farest_page_num() {
+	int res = -1; int tmpMax = -1;
+	int distance[4];
+	for (int i = 0; i < 4; i++) {
+		distance[i] = ::distance(pageNum(memoryQueue[i]));
+	}
+	for (int i = 0; i < 4; i++) {
+		if (distance[i] > tmpMax) {
+			tmpMax = distance[i];
+			res = i;
+		}
+	}
+	return pageNum(memoryQueue[res]);
+}
+
+void remove_page(int pageNum) {
+	deque<int*> tmpQue;
+	int* tmp;
+	while (1) {
+		tmp = memoryQueue.back();
+		if (::pageNum(tmp) == pageNum) {
+			memoryQueue.pop_back();
+			break;
+		}
+		else {
+			tmpQue.push_back(tmp);
+			memoryQueue.pop_back();
+		}
+	}
+	while (!tmpQue.empty()) {
+		int* temp = tmpQue.back();
+		tmpQue.pop_back();
+		memoryQueue.push_back(temp);
+	}
+}
+//1 12 2 8 ||17 1 15 2 13 24 3 1 16 2
+//dis(1)=1
+//dis(12)>32
+//dis(2)=3
+//dis(8)=32
+void run_OPT() {
+	while (pc != 320) {
+		cout << "当前指令序号:" << instruction[pc] << endl;
+		//不用置换
+		if (is_page_in_memoryQueue()) {}
+		//置换
+		else {
+			if (memoryQueue.size() < 4) {
+				int* finded = find_instruction(instruction[pc]);
+				memoryQueue.push_back(page[pageNum(finded)]);
+			}
+			else {
+				int farestPageNum = get_farest_page_num();
+				remove_page(farestPageNum);
+				int* finded = find_instruction(instruction[pc]);
+				memoryQueue.push_back(page[pageNum(finded)]);
+			}
+			print_memoryQueue();
+			numOfMissPage++;
+			pageCounter++;
+		}
+		pc++;
+	}
+	cout << "320条指令模拟完毕 置换方式<OPT> 缺页率:" << numOfMissPage / 3.2 << "%" << endl;
+}
+
+void run_LRU() {
+	while (pc != 320) {
+		cout << "当前指令序号:" << instructions[pc] << endl;
+		//不用置换
+		if (is_page_in_memoryQueue()) {
+			int counter = 0; int pageNumber = -1;
+			//find number
+			for (const auto& e : memoryQueue) {
+				for (int i = 0; i < 10; i++) {
+					if (e[i] == instruction[pc]) {
+						pageNumber = counter;
+					}
+				}
+				counter++;
+			}
+
+			deque<int*> tmpQue;
+			int* tmp;
+			while (1) {
+				tmp = memoryQueue.back();
+				if (pageNum(tmp) == pageNumber) {
+					memoryQueue.pop_back();
+					break;
+				}
+				else {
+					tmpQue.push_back(tmp);
+					memoryQueue.pop_back();
+				}
+			}
+			while (!tmpQue.empty()) {
+				int* temp = tmpQue.back();
+				tmpQue.pop_back();
+				memoryQueue.push_back(temp);
+			}
+			memoryQueue.push_back(tmp);
+		}
+		//
+		else {
+			if (memoryQueue.size() < 4) {
+				int* finded = find_instruction(pc);
+				memoryQueue.push_back(page[pageNum(finded)]);
+			}
+			else {
+				memoryQueue.pop_front();
+				int* finded = find_instruction(instruction[pc]);
+				memoryQueue.push_back(page[pageNum(finded)]);
+			}
+			print_memoryQueue();
+			numOfMissPage++;
+		}
+		pc++;
+	}
+	cout << "320条指令模拟完毕 置换方式<LRU> 缺页率:" << numOfMissPage / 3.2 << "%" << endl;
+}
+
+void run_FIFO() {
+
+	while (pc != 320) {
+		cout << "当前指令序号:" << instruction[pc] << endl;
+		//不用置换
+		if (is_page_in_memoryQueue()) {}
+		//置换
+		else {
+			if (memoryQueue.size() < 4) {
+				int* finded = find_instruction(instruction[pc]);
+				memoryQueue.push_back(page[pageNum(finded)]);
+				path.push_back(pageNum(finded));
+			}
+			else {
+				memoryQueue.pop_front();
+				int* finded = find_instruction(instruction[pc]);
+				memoryQueue.push_back(page[pageNum(finded)]);
+				path.push_back(pageNum(finded));
+			}
+
+			print_memoryQueue();
+			numOfMissPage++;
+		}
+		pc++;
+	}
+	cout << "320条指令模拟完毕 置换方式<FIFO> 缺页率:" << numOfMissPage / 3.2 << "%" << endl;
+}
+
+
+void execute(int style) {
+	reset();
+	switch (style) {
+	case OPT:
+		run_OPT();
+		break;
+
+	case FIFO:
+		run_FIFO();
+		break;
+
+	case LRU:
+		run_LRU();
+		break;
+
+	default:
+		std::cout << "illegal style\n";
+		break;
+	}
+}
+
+void HomeWork2() {
+	init_instruction();
+	execute(FIFO);
+	execute(LRU);
+	execute(OPT);
+}
+
+#endif /* Pages_h */
