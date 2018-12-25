8016bfe228bfef215bb776faa522e2c8e2e3afc7
292236116 <292236116@qq.com>
2018-12-25 11:47:27 +0800

    0

--- Gadget/STL Format.h
+++ Gadget/STL Format.h
@@ -1,5 +1,5 @@
 #pragma once
-//STL API FORM
+//public
 /*
 swap()
 max_size()
@@ -17,7 +17,11 @@ pCompare:函数指针 bool (*id)(args) 或者typeof,也就是decltype()
 
 using namespace std;
 #include<string>
-//假设我有一些对象,我使用不同的方式来处理它们
+#include <vector>
+#include <set>
+#include<unordered_set>
+#include<algorithm>
+
 struct Obj {
 	string describe;
 	int value;
@@ -26,8 +30,6 @@ struct Obj {
 		return l.describe == r.describe;
 	}
 };
-
-//记得要声明为常函数
 class ObjCmp {
 public:
 	bool operator()(const Obj& l, const Obj& r) {
@@ -46,24 +48,17 @@ public:
 		return std::hash<string>()(o.describe);
 	}
 };
-
-//如果你只是希望把它们存起来
-#include <vector>
 vector<Obj> vObj{ {"Hu",0,0},{"Wife",100,2},{"me",120,4} };
-//如果你希望索引它们
-#include <set>
-//set 需要一个函数指针CMP,默认为<,即从小到大,且key是const
+
+//set----order by key
+//key为const,当Cmp为<时,从小到大排序,with O(log)
 set<Obj, ObjCmp> sObj{ vObj.begin(),vObj.end()};
 
-//如果你希望映射它们,你需要定义==和调用哪一个std::hash<T>()
-#include<unordered_set>
+//hash----map by key
+//key为const,with O(1)
 unordered_set < Obj, ObjHash,ObjEq > hObj{ vObj.begin(),vObj.end() };
 
-#include <iostream>
-
 void doIt() {
-	
-	
-	
+
 }
 
